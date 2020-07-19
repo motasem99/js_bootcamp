7b9e9d351c5792fabca86b94d8129d1803663162
mutasem <motasemimad10@gmail.com>
2020-07-19 18:19:09 +0300

    js_bootcamp

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,4 +1,17 @@
-const notes = ['note 1', 'note 2', 'note 3'];
+const notes = [
+  {
+    title: 'my next trip',
+    body: 'i would like to go to spain',
+  },
+  {
+    title: 'habits to work on',
+    body: 'exercise, eating a bit better',
+  },
+  {
+    title: 'office modification',
+    body: 'get a new seat',
+  },
+];
 
 // console.log(notes.pop());
 // notes.push('my new note');
@@ -9,22 +22,55 @@ const notes = ['note 1', 'note 2', 'note 3'];
 // notes.splice(1, 0, 'note 4', 'note 5');
 // notes.splice(1, 1, 'note 5');
 
-notes[2] = 'this is now new note 3';
+////////
 
-notes.forEach(function (item, index) {
-  console.log(index);
-  console.log(item);
-});
+// notes[2] = 'this is now new note 3';
 
-console.log(notes.length);
-console.log(notes);
+// notes.forEach(function (item, index) {
+//   console.log(index);
+//   console.log(item);
+// });
 
 // counting... 1
 
-for (let count = 0; count <= 2; count++) {
-  console.log(count);
-}
+// for (let count = 0; count <= 2; count++) {
+//   console.log(count);
+// }
 
-for (let count = 0; count < notes.length; count++) {
-  console.log(notes[count]);
-}
+// for (let count = 0; count < notes.length; count++) {
+//   console.log(notes[count]);
+// }
+
+const findNote = function (notes, noteTitle) {
+  return notes.find(function (note, index) {
+    return note.title === noteTitle;
+  });
+};
+
+const findNotes = function (notes, query) {
+  return notes.filter(function (note, index) {
+    const isTitleMatch = note.title.includes(query.toLowerCase());
+    const isBodyMatch = note.body.includes(query.toLowerCase());
+    return isTitleMatch || isBodyMatch;
+  });
+};
+
+console.log(findNotes(notes, 'work'));
+// const findNote = function (note, noteTitle) {
+//     const index = notes.findIndex(function (note, index) {
+//       return note.title === noteTitle;
+//     });
+//     return notes[index];
+//   };
+
+const note = findNote(notes, 'office modification');
+console.log(note);
+
+// console.log(notes.length);
+// console.log(notes);
+
+// const index = notes.findIndex(function (note, index) {
+//   console.log(note);
+//   return note.title === 'habits to work on';
+// });
+// console.log(index);
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,32 +1,77 @@
-const todo = [
-  'order cat food',
-  'clean kitchen',
-  'buy food',
-  'do work',
-  'drink water',
-];
+// const todo = [
+//   'order cat food',
+//   'clean kitchen',
+//   'buy food',
+//   'do work',
+//   'drink water',
+// ];
 
-todo.splice(2, 1);
-todo.push('lets walk');
-todo.shift();
+// todo.splice(2, 1);
+// todo.push('lets walk');
+// todo.shift();
 
-console.log(`you have ${todo.length} todo`);
+// console.log(`you have ${todo.length} todo`);
 
-todo.forEach(function (item, index) {
-  const num = index + 1;
-  console.log(`${num}: ${item} `);
-});
+// todo.forEach(function (item, index) {
+//   const num = index + 1;
+//   console.log(`${num}: ${item} `);
+// });
 
-console.log('++++++++++++++++++++++++++++++++');
+// console.log('++++++++++++++++++++++++++++++++');
 
-for (let count = 0; count < todo.length; count++) {
-  const num = count + 1;
-  //   console.log(todo[count]);
-  console.log(`${num}: ${todo[count]}`);
-}
+// for (let count = 0; count < todo.length; count++) {
+//   const num = count + 1;
+//   //   console.log(todo[count]);
+//   console.log(`${num}: ${todo[count]}`);
+// }
 
 // console.log(`todo: ${todo[0]}`);
 // console.log(`todo: ${todo[todo.length - 2]}`);
 // console.log(`todo: ${todo[todo.length - 3]}`);
 // console.log(`todo: ${todo[todo.length - 4]}`);
 // console.log(`todo: ${todo[todo.length - 5]}`);
+
+const todo = [
+  {
+    text: 'order cat food',
+    completed: true,
+  },
+  {
+    text: 'clean kitchen',
+    completed: false,
+  },
+  {
+    text: 'buy food',
+    completed: true,
+  },
+  {
+    text: 'do work',
+    completed: false,
+  },
+  {
+    text: 'exercise',
+    completed: true,
+  },
+];
+// 1. convert array to arry of objects -> text , complete
+// 2. create function to remove a todo by text value
+
+const deleteTodos = function (todo, todoText) {
+  const index = todo.findIndex(function (todos) {
+    return todos.text === todoText;
+  });
+  if (index > -1) {
+    todo.splice(index, 1);
+  }
+};
+
+// deleteTodos(todo, 'buy food');
+// console.log(todo);
+
+const getThingsTodos = function (todo) {
+  return todo.filter(function (todos) {
+    return todos.completed === false;
+  });
+};
+
+console.log(getThingsTodos(todo));
