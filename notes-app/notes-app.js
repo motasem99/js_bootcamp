e36e59ba09d3b3e76b41a6b19f7978b32307ba6f
mutasem <motasemimad10@gmail.com>
2020-07-24 19:04:26 +0300

    js_bootcamp

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,47 +1,83 @@
-const notes = [
-  {
-    title: 'my next trip',
-    body: 'i would like to go to spain',
-  },
-  {
-    title: 'habits to work on',
-    body: 'exercise, eating a bit better',
-  },
-  {
-    title: 'office modification',
-    body: 'get a new seat',
-  },
+let notes = [
+  // {
+  //   title: 'my next trip',
+  //   body: 'i would like to go to spain',
+  // },
+  // {
+  //   title: 'habits to work on',
+  //   body: 'exercise, eating a bit better',
+  // },
+  // {
+  //   title: 'office modification',
+  //   body: 'get a new seat',
+  // },
 ];
 
 const filters = {
   searchText: '',
 };
 
-const renderNotes = function (notes, filers) {
+// check for existing saved data
+
+const notesJSON = localStorage.getItem('notes');
+
+if (notesJSON !== null) {
+  notes = JSON.parse(notesJSON);
+}
+
+const renderNotes = function (notes, filters) {
   const filteredNotes = notes.filter(function (note) {
     return note.title.toLowerCase().includes(filters.searchText.toLowerCase());
   });
+
+  document.querySelector('#notes').innerHTML = '';
+
+  filteredNotes.forEach(function (note) {
+    const noteEl = document.createElement('p');
+
+    if (note.title.length > 0) {
+      noteEl.textContent = note.title;
+    } else {
+      noteEl.textContent = 'unnamed note';
+    }
+
+    noteEl.textContent = note.title;
+    document.querySelector('#notes').appendChild(noteEl);
+  });
 };
-renderNotes(notes.filters);
+renderNotes(notes, filters);
 
 document.querySelector('#createNote').addEventListener('click', function (e) {
   // e.target.textContent = 'the button was clicked';
-  const newParagraph = document.createElement('p');
-  newParagraph.textContent = 'this is new paragraph from js';
-  document.querySelector('body').appendChild(newParagraph);
+  notes.push({
+    title: '',
+    body: '',
+  });
+
+  localStorage.setItem('notes', JSON.stringify(notes));
+  renderNotes(notes, filters);
+  // const newParagraph = document.createElement('p');
+  // newParagraph.textContent = 'this is new paragraph from js';
+  // document.querySelector('body').appendChild(newParagraph);
 });
 
-document.querySelector('#removeAll').addEventListener('click', function () {
-  document.querySelectorAll('.note').forEach(function (note) {
-    note.remove();
-  });
-});
+// document.querySelector('#removeAll').addEventListener('click', function () {
+//   document.querySelectorAll('.note').forEach(function (note) {
+//     note.remove();
+//   });
+// });
 
 document.querySelector('#search-text').addEventListener('input', function (e) {
   // console.log(e.target.value);
   filters.searchText = e.target.value;
   renderNotes(notes, filters);
 });
+
+// document.querySelector('#name-form').addEventListener('submit', function (e) {
+//   e.preventDefault();
+//   console.log(e.target.elements.firstName.value);
+//   e.target.elements.firstName.value = '';
+// });
 
 // // DOM - document object model
 
@@ -63,3 +99,12 @@ document.querySelector('#search-text').addEventListener('input', function (e) {
 // const newParagraph = document.createElement('p');
 // newParagraph.textContent = 'this is new paragraph from js';
 // document.querySelector('body').appendChild(newParagraph);
+
+///////////////////
+// document.querySelector('#for-fun').addEventListener('change', function (e) {
+//   console.log(e.target.checked);
+// });
+
+document.querySelector('#filter-by').addEventListener('change', function (e) {
+  console.log(e.target.value);
+});
--- todo-app/todo.js
+++ todo-app/todo.js
@@ -1,41 +1,68 @@
-const todo = [
-  {
-    text: 'order cat food',
-    completed: true,
-  },
-  {
-    text: 'clean kitchen',
-    completed: false,
-  },
-  {
-    text: 'buy food',
-    completed: true,
-  },
-  {
-    text: 'do work',
-    completed: false,
-  },
-  {
-    text: 'exercise',
-    completed: true,
-  },
+let todos = [
+  // {
+  //   text: 'order cat food',
+  //   completed: true,
+  // },
+  // {
+  //   text: 'clean kitchen',
+  //   completed: false,
+  // },
+  // {
+  //   text: 'buy food',
+  //   completed: true,
+  // },
+  // {
+  //   text: 'do work',
+  //   completed: false,
+  // },
+  // {
+  //   text: 'exercise',
+  //   completed: true,
+  // },
 ];
 
-const inCompleteTodos = todo.filter(function (todo) {
-  return !todo.completed;
-});
+const todosJSON = localStorage.getItem('todos');
+if (todosJSON !== null) {
+  todos = JSON.parse(todosJSON);
+}
 
-const summary = document.createElement('h2');
-summary.textContent = `you have ${inCompleteTodos.length} todo left`;
-document.querySelector('body').appendChild(summary);
+// 1. setup a div contain for todos
+// 2. setup filters (searchText) and wir up a new filters input to change it
+// 3. create a renderTodos function to render and render the latest filtered data
 
-todo.forEach(function (todo) {
-  const p = document.createElement('p');
-  p.textContent = todo.text;
-  document.querySelector('body').appendChild(p);
-});
+const filters = {
+  searchText: '',
+  hideCompleted: false,
+};
 
-/////////////////////
+const renderTodos = function (todos, filters) {
+  const filteredTodos = todos.filter(function (todo) {
+    const searchTextMatch = todo.text
+      .toLowerCase()
+      .includes(filters.searchText.toLowerCase());
+    const hideCompletedMatch = !filters.hideCompleted || !todo.completed;
+
+    return searchTextMatch && hideCompletedMatch;
+  });
+
+  const inCompleteTodos = filteredTodos.filter(function (todo) {
+    return !todo.completed;
+  });
+
+  document.querySelector('#todos').innerHTML = '';
+
+  const summary = document.createElement('h2');
+  summary.textContent = `you have ${inCompleteTodos.length} todo left`;
+  document.querySelector('#todos').appendChild(summary);
+
+  filteredTodos.forEach(function (todo) {
+    const p = document.createElement('p');
+    p.textContent = todo.text;
+    document.querySelector('#todos').appendChild(p);
+  });
+};
+
+renderTodos(todos, filters);
 
 // const paragraphs = document.querySelectorAll('p');
 
@@ -47,15 +74,53 @@ todo.forEach(function (todo) {
 
 // listen for new todo creation
 
-document.querySelector('#add-todo').addEventListener('click', function (e) {
-  e.target.textContent = 'was clicked babe';
-  const addH1 = document.createElement('h1');
-  addH1.textContent = 'this is h1';
-  document.querySelector('body').appendChild(addH1);
+// document.querySelector('#add-todo').addEventListener('click', function (e) {
+//   e.target.textContent = 'was clicked babe';
+//   const addH1 = document.createElement('h1');
+//   addH1.textContent = 'this is h1';
+//   document.querySelector('body').appendChild(addH1);
+// });
+
+// listen for todo text change
+
+// document
+//   .querySelector('#add-todo-text')
+//   .addEventListener('input', function (e) {
+//     console.log(e.target.value);
+//   });
+///////////////////////////
+document.querySelector('#search-text').addEventListener('input', function (e) {
+  filters.searchText = e.target.value;
+  renderTodos(todos, filters);
 });
 
+// 1. create a form with a single input for todo text
+// 2. setup an submit handler and cancel the default action
+// 3. add a new item to the todos array with that text data (completed value of false)
+// 4. rerender the application
+// 5. clear the input field value
+
+document.querySelector('#new-todo').addEventListener('submit', function (e) {
+  e.preventDefault();
+  todos.push({
+    text: e.target.elements.text.value,
+    completed: false,
+  });
+
+  localStorage.setItem('todos', JSON.stringify(todos));
+
+  renderTodos(todos, filters);
+  e.target.elements.text.value = '';
+});
+
+// 1. create a checkbox and and setup event listener -> "hide completed"
+// 2. create new hideCompleted filter (default false)
+// 3. update hideCompleted an rerender list on checkbox change
+// 4. setup rerenderTodos to remove completed items
+
 document
-  .querySelector('#add-todo-text')
-  .addEventListener('input', function (e) {
-    console.log(e.target.value);
+  .querySelector('#hide-completed')
+  .addEventListener('change', function (e) {
+    filters.hideCompleted = e.target.checked;
+    renderTodos(todos, filters);
   });
